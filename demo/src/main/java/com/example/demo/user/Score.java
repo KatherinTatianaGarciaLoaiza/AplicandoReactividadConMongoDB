f2a4c8d120960fe4899ed5252b020a954884c4d9
unknown <ktatiana.garcia@udea.edu.co>
2021-05-14 12:46:38 -0500

    Students

---
+++ demo/src/main/java/com/example/demo/user/Score.java
@@ -0,0 +1,22 @@
+package com.example.demo.user;
+
+public class Score {
+    double score;
+    String type;
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
